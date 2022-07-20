d800b563c300a108af37965abc3026c1fa18aeb4
Ariel Wolfe <arielwolfe95@gmail.com>
2022-07-19 20:22:17 -0700

    Fall back route and controller to override 404

--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostsController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\FallbackController;
 
 //forward slash is optional
 
@@ -11,17 +12,14 @@ use App\Http\Controllers\HomeController;
 //Invoke Controller
 Route::get('/', HomeController::class);
 
-//keep / if its the same as prefix
-//otherwise first / can be removed if more following prefix
 Route::prefix('/blog')->group(function () {
-  //ex this get is '/blog' so blog removed but first / stays
-Route::get('/', [PostsController::class, 'index'])->name('blog.index');
-Route::get('/{id}', [PostsController::class, 'show'])->name('blog.show');
-Route::get('/create', [PostsController::class, 'create'])->name('blog.create');
-Route::post('/', [PostsController::class, 'store'])->name('blog.store');
-Route::get('/edit/{id}', [PostsController::class, 'edit'])->name('blog.edit');
-Route::patch('/{id}', [PostsController::class, 'update'])->name('blog.update');
-Route::delete('/{id}', [PostsController::class, 'destroy'])->name('blog.delete');
+  Route::get('/', [PostsController::class, 'index'])->name('blog.index');
+  Route::get('/{id}', [PostsController::class, 'show'])->name('blog.show');
+  Route::get('/create', [PostsController::class, 'create'])->name('blog.create');
+  Route::post('/', [PostsController::class, 'store'])->name('blog.store');
+  Route::get('/edit/{id}', [PostsController::class, 'edit'])->name('blog.edit');
+  Route::patch('/{id}', [PostsController::class, 'update'])->name('blog.update');
+  Route::delete('/{id}', [PostsController::class, 'destroy'])->name('blog.delete');
 });
 
 /*
@@ -32,3 +30,7 @@ Route::any('/blog', [PostsController::class, 'index']);
 //Return View
 Route::view('/blog', 'blog.index', ['name' => 'code with ariel']);
 */
+
+//Fallback Route
+//do not need to define method/end point
+Route::fallback(FallbackController::class);
